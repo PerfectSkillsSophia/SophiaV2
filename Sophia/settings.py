863f87f3bd6382747ad6fa7c5a457b181bfca5ce
PerfectSkillsSophia <tech@perfectskills.in>
2024-01-05 12:04:25 +0530

    testing

--- Sophia/settings.py
+++ Sophia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hwqdmkwi_15ufti4h^egn!e8!dp=bk7ek@qb8gwf7b*c5dvp0i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.71.236.84','*']
 
 
 # Application definition
