91960d176091afe188be4790695a4615ecedeff5
PerfectSkillsSophia <tech@perfectskills.in>
2024-01-07 19:03:50 +0530

    testing

--- Sophia/settings.py
+++ Sophia/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Accounts',
     'Administration',
     'Assessments',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Sophia.urls'
